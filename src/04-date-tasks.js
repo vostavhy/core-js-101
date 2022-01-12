0262fba0da0922dfd8683287fb7b52edc546c721
vostavhy <vostavhy@gmail.com>
2022-01-12 14:48:19 +0300

    feat: implement hours

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -17,8 +17,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -32,8 +32,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 /**
@@ -50,8 +50,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  date.setDate(29);
+  return date.getDate() === 29;
 }
 
 /**
@@ -69,8 +70,9 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const date = new Date(endDate - startDate);
+  return date.toISOString().split('T')[1].replace('Z', '');
 }
 
 /**
@@ -89,8 +91,14 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  let tempHours = date.getUTCHours();
+  if (tempHours >= 12) tempHours -= 12;
+  const hours = 0.5 * (60 * tempHours + date.getUTCMinutes());
+  const minutes = 6 * date.getUTCMinutes();
+  let degrees = Math.abs(hours - minutes);
+  if (degrees > 180) degrees -= 180;
+  return (degrees * Math.PI) / 180;
 }
 
 module.exports = {
